287b54cd43fa21a5f25841b01abae693cd372014
Diego Andres Yave Guzman <diego.yave@neoris.com>
2022-04-28 18:06:42 +0200

    Add project files.

---
+++ GenericMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GenericMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
