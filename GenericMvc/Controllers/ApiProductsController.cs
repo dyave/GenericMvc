f038dd750d4376d6db93d04f86bbf4fa2bc541b5
Diego Andres Yave Guzman <diego.yave@neoris.com>
2022-05-23 11:38:04 +0200

    Add API product with scaffold and query script

---
+++ GenericMvc/Controllers/ApiProductsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GenericMvc.Context;
+using GenericMvc.Models;
+
+namespace GenericMvc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiProductsController : ControllerBase
+    {
+        private readonly AdventureWorksLT2019Context _context;
+
+        public ApiProductsController(AdventureWorksLT2019Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ApiProducts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProduct()
+        {
+            return await _context.Product.ToListAsync();
+        }
+
+        // GET: api/ApiProducts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Product.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        // PUT: api/ApiProducts/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct(int id, Product product)
+        {
+            if (id != product.ProductId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ApiProducts
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+            _context.Product.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduct", new { id = product.ProductId }, product);
+        }
+
+        // DELETE: api/ApiProducts/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Product>> DeleteProduct(int id)
+        {
+            var product = await _context.Product.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Product.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Product.Any(e => e.ProductId == id);
+        }
+    }
+}
---
+++ resources/AdventureWorks.sql
@@ -0,0 +1,25 @@
+
+
+
+select * from SalesLT.ProductCategory;
+
+SELECT 'SELECT * FROM ' + TABLE_SCHEMA + '.' + TABLE_NAME + ';', * FROM INFORMATION_SCHEMA.TABLES;
+
+SELECT * FROM dbo.ErrorLog;
+SELECT * FROM dbo.BuildVersion;
+---
+SELECT * FROM SalesLT.Address;
+SELECT * FROM SalesLT.Customer;
+SELECT * FROM SalesLT.CustomerAddress;
+SELECT * FROM SalesLT.Product;
+SELECT * FROM SalesLT.ProductCategory;
+SELECT * FROM SalesLT.ProductDescription;
+SELECT * FROM SalesLT.ProductModel;
+SELECT * FROM SalesLT.ProductModelProductDescription;
+SELECT * FROM SalesLT.SalesOrderDetail;
+SELECT * FROM SalesLT.SalesOrderHeader;
+---
+SELECT * FROM SalesLT.vProductAndDescription;
+SELECT * FROM SalesLT.vProductModelCatalogDescription;
+SELECT * FROM SalesLT.vGetAllCategories;
+
